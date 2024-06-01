98771491c4d2f5bad64ccaacb4c1d79e1e650f43
jeff0518 <cycivs07@icloud.com>
2024-06-01 11:44:10 +0800

    feat: add index.js

---
+++ Pixel Art Generator/index.js
@@ -0,0 +1,43 @@
+// 操作 DOM element
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementById("erase-btn");
+let paintBtn = document.getElementById("paint-btn");
+let widthValue = document.getElementById("width-value");
+let heightValue = document.getElementById("height-value");
+
+// 宣告 events ，裡面是 mouse event跟 touch event
+let events = {
+  mouse: {
+    down: "mousedown",
+    move: "mousemove",
+    up: "mouseup",
+  },
+  touch: {
+    down: "touchstart",
+    move: "touchmove",
+    up: "touchend",
+  },
+};
+
+let deviceType = "";
+let draw = false;
+let erase = false;
+
+// 判斷此次操作是否是touch
+const isTouchDevice = () => {
+  try {
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (e) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+isTouchDevice();
